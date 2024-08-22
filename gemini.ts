d3e8676b57d185d4cd1dc01ef88f5391abbc4488
Charles Arowolo <charles19rex@gmail.com>
2024-08-22 15:38:08 +0100

    ai

--- gemini.ts
+++ gemini.ts
@@ -1,10 +1,11 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY!;
 
-const genAI = new GoogleGenerativeAI(apiKey);
+const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+console.log(apiKey);
+const genAI = new GoogleGenerativeAI(apiKey!);
 
 export const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-pro-latest",
+  model: "gemini-1.5-flash",
   systemInstruction:
     "You are an internationally renowned chef with specialized expertise in a vast array of global cuisines. Your profound culinary knowledge enables you to provide not only detailed recipes but also insights into cooking techniques and the cultural significance of dishes from around the world. As a culinary mentor, your role is to guide users through the art of cooking, sharing the history and traditions that make each recipe special. When responding, I will give you a template to structure your output, using placeholders such as Dish, Ingredients, Steps, Instructions of equipment, and Utensils. Please fit your responses into this template, preserving its formatting and structure to offer clear, organized, and comprehensive culinary guidance.",
 });
@@ -41,7 +42,12 @@ export const mealChat = {
 };
 
 export async function chef(message: string): Promise<string> {
-  mealChat.initChat(model, config);
-  const response = await mealChat.sendMessage(message);
-  return response;
+  try {
+    mealChat.initChat(model, config);
+    const response = await mealChat.sendMessage(message);
+    return response;
+  } catch (error) {
+    console.log(error);
+    return "Error";
+  }
 }
