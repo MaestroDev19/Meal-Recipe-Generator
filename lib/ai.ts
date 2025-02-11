5784bf4b83a96c6538bb46ab3ca6f7ff65718224
Charles Damilola Arowolo <charles19rex@gmail.com>
2025-02-11 17:07:39 +0000

    Update ai.ts

--- lib/ai.ts
+++ lib/ai.ts
@@ -2,7 +2,7 @@ import { Groq } from "groq-sdk";
 const apiKey = process.env.NEXT_PUBLIC_GROQ_API_KEY;
 
 const groq = new Groq({
-  apiKey: apiKey!,dangerouslyAllowBrowser: true,"model": "llama-3.3-70b-versatile",
+  apiKey: apiKey!,dangerouslyAllowBrowser: true
 });
 
 
@@ -42,7 +42,7 @@ Utensils: [List of required utensils]` }, { role: "user", content: prompt }],
     recipe = response.choices[0].message.content!;
   } else {
     const response = await groq.chat.completions.create({
-      model: "llama-3.2-11b-text-preview",
+     "model": "llama-3.3-70b-versatile",
       messages: [{ role: "system", content: `You are an internationally renowned chef with deep expertise in a wide range of global cuisines. Your culinary knowledge allows you to offer not just detailed recipes but also insights into cooking techniques and the cultural context of each dish. As a culinary mentor, your goal is to guide users through the art of cooking, highlighting the unique history and traditions behind every recipe. add  the kcal and the protein, fat, and carbs for each ingredient.add  the kcal and the protein, fat, and carbs for each ingredient. Suggest substitutes for missing ingredients. Allow users to rate the suggested recipe or ask for a revision. Check if the entered ingredients are valid food items.Break down steps into clear instructions When responding, use the following structured template:
 
 Dish: [Name of the dish]
